9c65e987bd0dd7b1ef6f84134271e090eebe3089
FaceNach <ignacioijg@gmail.com>
2024-05-10 18:27:51 -0300

    Added models to the project and also connection to MongoDB

--- SuggestionAppUI/obj/Debug/net8.0/SuggestionAppUI.AssemblyInfo.cs
+++ SuggestionAppUI/obj/Debug/net8.0/SuggestionAppUI.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("a6b70f86-dc51-47b3-89fd-ab895ec4ee70")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuggestionAppUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd40de63ab0bb606f56c50cf0e9e109bfd9cfd44")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuggestionAppUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuggestionAppUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
